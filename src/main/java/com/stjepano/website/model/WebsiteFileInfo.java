ff11532bbd9bfa970fff78a940cf9ea55a07f5d9
stjepano <stjepano@gmail.com>
2017-05-29 13:19:50 +0100

    Files service - work in progress

---
+++ src/main/java/com/stjepano/website/model/WebsiteFileInfo.java
@@ -0,0 +1,135 @@
+package com.stjepano.website.model;
+
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
+
+/**
+ * Information about a file ...
+ *
+ * Note that file paths are relative paths in upload folder, not the full path with upload folder.
+ * The path also starts with /
+ *
+ * Directory path should end with /
+ *
+ * Mime type will be null for non regular files!
+ */
+public class WebsiteFileInfo {
+    private final String path;
+    private final boolean dir;
+    private final boolean regularFile;
+    private final Date lastModifiedTime;
+    private final String mimeType;
+
+    private WebsiteFileInfo(String path, boolean dir, boolean regularFile, Date lastModifiedTime, String mimeType) {
+        this.mimeType = mimeType;
+        if (lastModifiedTime == null) throw new NullPointerException();
+        if (!path.startsWith("/")) {
+            throw new IllegalArgumentException("path must start with /");
+        }
+        if (dir && !path.endsWith("/")) {
+            throw new IllegalArgumentException("directory paths must end with /");
+        }
+        if (!dir && path.endsWith("/")) {
+            throw new IllegalArgumentException("non-directory paths must not end with /");
+        }
+        this.path = path;
+        this.dir = dir;
+        this.regularFile = regularFile;
+        this.lastModifiedTime = lastModifiedTime;
+    }
+
+    /** Get relative path (path is relative to upload folder) */
+    public String getPath() {
+        return path;
+    }
+
+    /** Return true if directory */
+    public boolean isDir() {
+        return dir;
+    }
+
+    /** Return true if file is regular file */
+    public boolean isRegularFile() {
+        return regularFile;
+    }
+
+    /** Gets date when file was updated */
+    public Date getLastModifiedTime() {
+        return lastModifiedTime;
+    }
+
+    /** Gets file mime type */
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    /** Return filename. If file is directory then return dir name */
+    public String getFilename() {
+        return FilenameUtils.getName(this.path);
+    }
+
+    /** Gets extension */
+    public String getExtension() {
+        String filename = getFilename();
+        return FilenameUtils.getExtension(filename);
+    }
+
+    /** Gets path of a file */
+    public String getParent() {
+        return FilenameUtils.getPath(this.path);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        WebsiteFileInfo that = (WebsiteFileInfo) o;
+
+        return path.equals(that.path);
+    }
+
+    @Override
+    public int hashCode() {
+        return path.hashCode();
+    }
+
+    public static WebsiteFileInfo createFromRealPath(String uploadFolder, Path realPath) {
+        if (!Files.exists(realPath)) {
+            throw new IllegalArgumentException("File '" + realPath.toString() + "' does not exist!");
+        }
+        boolean dir = Files.isDirectory(realPath);
+        boolean regularFile = Files.isRegularFile(realPath);
+        String mimeType = null;
+        if (Files.isRegularFile(realPath)) {
+            try {
+                mimeType = Files.probeContentType(realPath);
+            } catch (IOException e) {
+                throw new RuntimeException("Could not determine mime type of file '" + realPath.toString() + "'", e);
+            }
+        }
+        Date lastModifiedTime = null;
+        try {
+            lastModifiedTime = new Date(Files.getLastModifiedTime(realPath).toMillis());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Path uploadPath = Paths.get(uploadFolder).normalize().toAbsolutePath();
+        realPath = realPath.normalize().toAbsolutePath();
+        if (!realPath.startsWith(uploadPath)) {
+            throw new RuntimeException("Trying to create FileRecord from a file that is not in uploadFolder. File path: " + realPath.toAbsolutePath().toString() + ", upload path: " + uploadFolder);
+        }
+        Path relativeFilePath = uploadPath.relativize(realPath).normalize();
+        // now relativeFilePath should contain only interesting part to us
+        String resultName = "/" + relativeFilePath.toString();
+        if (dir && !resultName.endsWith("/")) {
+            resultName = resultName + "/";
+        }
+        return new WebsiteFileInfo(resultName, dir, regularFile, lastModifiedTime, mimeType);
+    }
+}
---
+++ src/main/java/com/stjepano/website/services/FileService.java
@@ -0,0 +1,99 @@
+package com.stjepano.website.services;
+
+import com.stjepano.website.components.WebsiteConfig;
+import com.stjepano.website.model.WebsiteFileInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * The file service handles all file operations for the website.
+ */
+@Service
+public class FileService implements ResourceLoaderAware {
+
+    @Autowired
+    private WebsiteConfig websiteConfig;
+
+    private ResourceLoader resourceLoader;
+
+    /** Upload file to given directory */
+    public void upload(MultipartFile file, String uploadPath) {
+
+
+    }
+
+    /** List all files in given directory */
+    public List<WebsiteFileInfo> list(String uploadPath) {
+        Path path = path(uploadPath);
+        try {
+            return Files.list(path).map(p -> WebsiteFileInfo.createFromRealPath(websiteConfig.getUploadFolder(), p)).collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException("Can not list content of '" + path.toString() + "' directory!", e);
+        }
+    }
+
+    /** Returns file info for give file, if file exists */
+    public Optional<WebsiteFileInfo> get(String uploadPath) {
+        Path path = path(uploadPath);
+        if (!Files.exists(path)) {
+            return Optional.empty();
+        }
+        return Optional.of(WebsiteFileInfo.createFromRealPath(websiteConfig.getUploadFolder(), path));
+    }
+
+    /** Returns resource for given file if file exista */
+    public Optional<Resource> getAsResource(String uploadPath) {
+        Path path = path(uploadPath);
+        if (!Files.exists(path)) {
+            return Optional.empty();
+        }
+        return Optional.of(resourceLoader.getResource("file:" + path.toAbsolutePath().toString()));
+    }
+
+    /** Deletes specified files */
+    public void delete(Set<String> files) {
+
+    }
+
+    /** Copies files to given path */
+    public void copyTo(Set<String> files, String uploadPath) {
+
+    }
+
+    /** Moves files to given upload path */
+    public void moveTo(Set<String> files, String uploadPath) {
+
+    }
+
+    /** Creates new directory */
+    public void createDirectory(String uploadPath) {
+
+    }
+
+    /** Deletes directory and all files in it */
+    public void deleteDirectory(String uploadPath) {
+
+    }
+
+    private Path path(String uploadPath) {
+        return Paths.get(websiteConfig.getUploadFolder(), uploadPath);
+    }
+
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+}
